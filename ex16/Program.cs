9e7b687234d9b1491bbed382b7de41fe8caa99c1
João Vitor <joaomoraes26@icloud.com>
2026-02-27 19:43:59 -0300

    Exercicio 16

---
+++ ex16/Program.cs
@@ -0,0 +1,40 @@
+using System;
+
+class ConversorTempo
+{
+    static void Main()
+    {
+        int totalSegundos = LerSegundos();
+
+        int horas = CalcularHoras(totalSegundos);
+        int minutos = CalcularMinutos(totalSegundos);
+        int segundos = CalcularSegundosRestantes(totalSegundos);
+
+        EscreverResultado(horas, minutos, segundos);
+    }
+
+        static int LerSegundos() // método que le o que foi escrito pelo usuario
+
+    {
+        Console.Write("Digite o tempo em segundos: ");
+        return int.Parse(Console.ReadLine()!);
+    }
+
+    static int CalcularHoras(int totalSegundos) // método que calcula as horas 
+    {
+        return totalSegundos / 3600;
+    }
+ 
+    static int CalcularMinutos(int totalSegundos)  // método que calcula os minutos
+    {
+        return (totalSegundos % 3600) / 60;
+    }
+    static int CalcularSegundosRestantes(int totalSegundos) // método que calcula os segundos 
+    {
+        return totalSegundos % 60;
+    }
+    static void EscreverResultado(int horas, int minutos, int segundos) // método que escreve o resultado
+    {
+        Console.WriteLine($"\n{horas} hora(s), {minutos} minuto(s) e {segundos} segundo(s).");
+    }
+}
